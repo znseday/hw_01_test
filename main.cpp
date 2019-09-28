4413f5a01cc56096880f08ba08a7baddcb568901
znseday <55926888+znseday@users.noreply.github.com>
2019-09-28 23:24:32 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include "lib.h"
 
-#include <spdlog/spdlog.h>
+//#include <spdlog/spdlog.h>
+
+using namespace std;
 
 int main()
 {
-    auto logger = spdlog::stdout_logger_mt("console");
-    logger->info("version {} was started", version());
+    //auto logger = spdlog::stdout_logger_mt("console");
+   // logger->info("version {} was started", version());
 
+    cout << "build " << version() << endl;
+    cout << "Hello, World!" << endl;
+    
     return 0;
 }
